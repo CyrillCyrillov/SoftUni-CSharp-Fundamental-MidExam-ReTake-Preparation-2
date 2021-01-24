b89e2e3914d2120e1099456964c42badcc30239e
Kiril Kirilov <kirilkirilov@abv.bg>
2021-01-24 23:13:10 +0200

    Add files via upload

---
+++ Task01_SoftUni_Reception/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Task01_SoftUni_Reception
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstLimitPerHour = int.Parse(Console.ReadLine());
+            int secondLimitPerHour = int.Parse(Console.ReadLine());
+            int thithLimitPerHour = int.Parse(Console.ReadLine());
+
+            int studentsPerHour = firstLimitPerHour + secondLimitPerHour + +thithLimitPerHour;
+
+            int allStudents = int.Parse(Console.ReadLine());
+
+            int neededHours = 0;
+
+            while (allStudents > 0)
+            {
+                neededHours++;
+                if(neededHours % 4 != 0)
+                {
+                    allStudents -= studentsPerHour;
+                }
+            }
+            
+            Console.WriteLine($"Time needed: {neededHours}h.");
+        }
+    }
+}
